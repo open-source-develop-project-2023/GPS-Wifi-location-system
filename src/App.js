4dd68ab058cefcf4b5d36aaede1fb39fea19fef8
mega-317 <mega31712@gmail.com>
2023-11-14 15:23:42 +0900

    build

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
+import React, { useState, useEffect } from 'react';
 import { Map, MapMarker } from 'react-kakao-maps-sdk';
 
 const App = () => {
-  const locations = [
-    { title: '카카오', latlng: { lat: 33.450705, lng: 126.570677 } },
-    { title: '생태연못', latlng: { lat: 33.450936, lng: 126.569477 } },
-    { title: '텃밭', latlng: {lat: 33.450879, lng: 126.56994 } },
-  ];
+  const [location, setLocation] = useState(null);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(successHandler, errorHandler);
+  }, []);
+
+  const successHandler = (response) => {
+    console.log(response);
+    const { latitude, longitude } = response.coords;
+    setLocation({ latitude, longitude });
+  };
+
+  const errorHandler = (error) => {
+    console.log(error);
+  };
 
   return (
-    <Map center={{ lat: 33.5563, lng: 126.79581 }} style={{ width: '800px', height: '600px'}} level={3}>
-      {locations.map((loc, idx) => (
-        <MapMarker 
-          key={'${loc.title}-&{loc.latlng}'}
-          position={loc.latlng}
-          image={{
-            src: 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png',
-            size: { width: 24, height: 35 },
-          }}
-          title={loc.title}
-        />
-      ))}
-    </Map>
+    <>
+      {location && (
+        <Map
+          center={{ lat: location.latitude, lng: location.longitude }}
+          style={{ width: '800px', height: '600px' }}
+          level={3}
+        >
+            <MapMarker position={{ lat: location.latitude, lng: location.longitude }} />
+        </Map>
+      )}
+    </>
   );
 };
 
