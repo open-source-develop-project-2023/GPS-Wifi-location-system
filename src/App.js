51db541f20cf034e8173403ec5e880fe6562a08c
mega-317 <mega31712@gmail.com>
2023-11-14 15:11:11 +0900

    kakao-map

--- src/App.js
+++ src/App.js
@@ -1,53 +1,27 @@
-import './App.css';
-import { useEffect } from 'react';
+import { Map, MapMarker } from 'react-kakao-maps-sdk';
 
-function App() {
-    
-  //스크립트 파일 읽어오기
-  const new_script = src => { 
-    return new Promise((resolve, reject) => { 
-      const script = document.createElement('script'); 
-      script.src = src; 
-      script.addEventListener('load', () => { 
-        resolve(); 
-      }); 
-      script.addEventListener('error', e => { 
-        reject(e); 
-      }); 
-      document.head.appendChild(script); 
-    }); 
-  };
-  
-  useEffect(() => { 
-    //카카오맵 스크립트 읽어오기
-    const my_script = new_script('https://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=f3bf6495216810e09bd6c6cfba23248c');
-    
-    //스크립트 읽기 완료 후 카카오맵 설정
-    my_script.then(() => { 
-      console.log('script loaded!!!');  
-      const kakao = window['kakao']; 
-      kakao.maps.load(() => {
-        const mapContainer = document.getElementById('map');
-        const options = { 
-          center: new kakao.maps.LatLng(37.56000302825312, 126.97540593203321), //좌표설정
-          level: 3 
-        }; 
-        const map = new kakao.maps.Map(mapContainer, options); //맵생성
-        //마커설정
-        const markerPosition = new kakao.maps.LatLng(37.56000302825312, 126.97540593203321); 
-        const marker = new kakao.maps.Marker({ 
-          position: markerPosition
-        }); 
-        marker.setMap(map); 
-      });   
-    }); 
-  }, []);
+const App = () => {
+  const locations = [
+    { title: '카카오', latlng: { lat: 33.450705, lng: 126.570677 } },
+    { title: '생태연못', latlng: { lat: 33.450936, lng: 126.569477 } },
+    { title: '텃밭', latlng: {lat: 33.450879, lng: 126.56994 } },
+  ];
 
   return (
-    <div className="App">
-      <div id="map" className="map"/>
-    </div>
+    <Map center={{ lat: 33.5563, lng: 126.79581 }} style={{ width: '800px', height: '600px'}} level={3}>
+      {locations.map((loc, idx) => (
+        <MapMarker 
+          key={'${loc.title}-&{loc.latlng}'}
+          position={loc.latlng}
+          image={{
+            src: 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png',
+            size: { width: 24, height: 35 },
+          }}
+          title={loc.title}
+        />
+      ))}
+    </Map>
   );
-}
+};
 
 export default App;
